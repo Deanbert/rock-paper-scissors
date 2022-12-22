29958395ca14f0f8d9bc384b9ce24dec76376a52
Deanbert <deanfay2000@gmail.com>
2022-12-22 01:41:27 +0000

    Amend JS after testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,61 @@
 //Selectors
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-const computerScore = document.getElementById('computer-score')
-const playerScore = document.getElementById('player-score')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+const computerScore = document.getElementById('computer-score');
+const playerScore = document.getElementById('player-score');
+let userChoice;
+let computerChoice;
+let result;
 
 //Event Listener
 //possibleChoice function taken from Ania Kubow as stated in readme file
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
-  generateComputerChoice()
-  getResult()
-}))
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice();
+  getResult();
+}));
 
 // Functions
 //generateComputerChoice and getResult functions taken from Ania Kubow as stated in readme file
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
   
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   }
   if (randomNumber === 2) {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
   if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
   if (computerChoice === userChoice) {
-    result = 'Its a draw! Try Again'
+    result = 'Its a draw! Try Again';
   }
   if (computerChoice === 'rock' && userChoice === "paper") {
-    result = 'You Win, Congratulations!'
+    result = 'You Win, Congratulations!';
   }
   if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = 'Hard Luck, Try Again!'
+    result = 'Hard Luck, Try Again!';
   }
   if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = 'Victory! Keep going for the Big Win.'
+    result = 'Victory! Keep going for the Big Win.';
   }
   if (computerChoice === 'paper' && userChoice === "rock") {
-    result = 'You Lost, but dont Give Up!'
+    result = 'You Lost, but dont Give Up!';
   }
   if (computerChoice === 'scissors' && userChoice === "rock") {
-    result = 'You Won, Show that Computer Whos Boss!'
+    result = 'You Won, Show that Computer Whos Boss!';
   }
   if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = 'Commiserations, You Lost!'
+    result = 'Commiserations, You Lost!';
   }
-  resultDisplay.innerHTML = result
+  resultDisplay.innerHTML = result;
 }
